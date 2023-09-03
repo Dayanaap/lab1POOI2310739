f2648d27666f20cba66e40afe7cf6f50e6ce5af5
Dayanaap <143909714+Dayanaap@users.noreply.github.com>
2023-09-02 20:47:01 -0500

    Create ejercicio1F.java

---
+++ ejercicio1F.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main
+{
+    	public static void main(String[] args) {
+    	    
+		int a1=100; int a2=200;
+		
+		if(a1>100 && a2<=200)
+		 System.out.print(a1+" "+a2+" "+(a1+a2));
+		else
+		 System.out.print(a1+" "+a2+" "+(2*a1-a2));
+		
+		// Respuesta : 100 200 0
+    }
+}
