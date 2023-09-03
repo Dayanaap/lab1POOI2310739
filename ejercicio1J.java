a90d83bcdf2883718837c03bd40ee6bb1c9941ce
Dayanaap <143909714+Dayanaap@users.noreply.github.com>
2023-09-02 20:39:59 -0500

    Create ejercicio1J.java

---
+++ ejercicio1J.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main
+{
+    	public static void main(String[] args) {
+    	    
+		int x=1, y=2, z=3;
+		
+		if (x < z)
+		  System.out.println("*");
+		else if(x==z)
+		  System.out.println("&");
+		else
+		  System.out.println("$");
+		 
+		 // Respuesta : *
+		
+    }
+}
