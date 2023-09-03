9358a2c10d49f33fe67dcd9e367b15a7b2e8ff53
Dayanaap <143909714+Dayanaap@users.noreply.github.com>
2023-09-02 19:57:50 -0500

    Create ejercicio1A.java

---
+++ ejercicio1A.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+		if(6<5*5)
+		 System.out.println("Hello");
+		 System.out.println("There");
+		
+		// Respuesta: Hello
+		// There
+		
+
+       
+    }
+}
